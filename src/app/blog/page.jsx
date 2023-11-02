91569638102fbb9c88fb128a4746ba43de13a931
Gavutham <kgavutham2005@gmail.com>
2023-11-02 18:33:24 +0530

    create portfolio and blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,57 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+import Image from "next/image";
+
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className={styles.container}>
+      <Link href="blog/blogId" className={styles.wrapper}>
+        <div className={styles.imgContainer}>
+          <Image
+            alt=""
+            src="https://images.pexels.com/photos/135018/pexels-photo-135018.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Blog Title</h1>
+          <p className={styles.desc}>Blog Desc</p>
+        </div>
+      </Link>
+      <Link href="blog/blogId" className={styles.wrapper}>
+        <div className={styles.imgContainer}>
+          <Image
+            alt=""
+            src="https://images.pexels.com/photos/135018/pexels-photo-135018.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Blog Title</h1>
+          <p className={styles.desc}>Blog Desc</p>
+        </div>
+      </Link>
+      <Link href="blog/blogId" className={styles.wrapper}>
+        <div className={styles.imgContainer}>
+          <Image
+            alt=""
+            src="https://images.pexels.com/photos/135018/pexels-photo-135018.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Blog Title</h1>
+          <p className={styles.desc}>Blog Desc</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import Button from "@/components/Button/Button";
+import styles from "./page.module.css";
+import Image from "next/image";
 
-const Category = () => {
-  return <div>Category</div>;
+const Category = ({ params }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text={"See More"} url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            src="https://images.pexels.com/photos/135018/pexels-photo-135018.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
