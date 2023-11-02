fc02a91d7ea41ffb6f8bf629e137a83e50a368a1
Gavutham <kgavutham2005@gmail.com>
2023-11-02 17:23:58 +0530

    create contact page and animations for image

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,34 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's be in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            alt="contact-img"
+            fill={true}
+            src={"/contact.png"}
+            className={styles.image}
+          />
+        </div>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name" className={styles.input} />
+          <input type="email" placeholder="Email" className={styles.input} />
+          <textarea
+            placeholder="Message"
+            cols="30"
+            rows="10"
+            className={styles.textArea}
+          ></textarea>
+          <Button url="#" text={"Send"} />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
